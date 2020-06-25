21ebf0a076a157332e2ced0ebc2220984f7bee75
Battiatus <50103241+Battiatus@users.noreply.github.com>
2020-06-25 19:24:39 +0200

    Update of function

--- handler.js
+++ handler.js
@@ -2,13 +2,15 @@ const puppeteer = require('puppeteer');
 const { getChrome } = require('./chrome-script');
 
 module.exports.Canary = async (event) => {
-  const {  } = event.queryStringParameters;
-  const chrome = await getChrome();
-  const browser = await puppeteer.connect({
-    browserWSEndpoint: chrome.endpoint,
-  });
+    const { } = event.queryStringParameters;
+    const chrome = await getChrome();
+    const browser = await puppeteer.connect({
+        browserWSEndpoint: chrome.endpoint,
+    });
+
+    let page = 'URL';
   const page = await browser.newPage();
-  await page.goto(url, { waitUntil: 'networkidle0' });
+  await page.goto('url', { waitUntil: 'networkidle0' });
   const content = await page.evaluate(() => document.body.innerHTML);
   return {
     statusCode: 200,
